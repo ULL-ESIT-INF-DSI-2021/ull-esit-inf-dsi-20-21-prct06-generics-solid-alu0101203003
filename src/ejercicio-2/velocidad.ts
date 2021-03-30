7b43fdf21dfe61738ee78ee5aea28a562c67a789
alu0101203003 <alu0101203003@ull.edu.es>
2021-03-30 16:13:47 +0100

    ej2: clase velocidad funciona

---
+++ src/ejercicio-2/velocidad.ts
@@ -0,0 +1,41 @@
+import {isConvertible} from "./isconvertible";
+
+/**
+ * Array que contiene los posibles cambios 
+ * (La conversion deberá coincidir con alguno de ellos)
+*/
+var cambiosVelocidad :string[] = ["Kilometros/hora a Metros/segundo","Metros/segundo a Kilometros/hora",
+                                "Kilometros/hora a Millas/hora","Millas/hora a Kilometros/hora"]
+
+export class Velocidad implements isConvertible<string[]> {
+    cambios = cambiosVelocidad;
+
+    constructor(public unidad :number){}
+
+    /**
+     * Funcion convert. 
+     * Convierte la unidad del objeto al que se aplica 
+     * en función de la conversion recibida como parámetro
+     * @param conversion
+     */
+    convert(conversion :string){
+        var resultado :number = 0;
+        switch (conversion) {
+            case this.cambios[0]:
+                resultado = this.unidad/3.6
+                break;
+            case this.cambios[1]:
+                resultado = this.unidad*3.6
+                break;
+            case this.cambios[2]:
+                resultado = this.unidad/1.609
+                break;
+            case this.cambios[3]:
+                resultado = this.unidad*1.609
+                break;
+            default:
+                break;
+        }
+        return resultado
+    }
+}
--- tests/ejercicio-2.spec.ts
+++ tests/ejercicio-2.spec.ts
@@ -1,10 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
 import {Longitud} from '../src/ejercicio-2/longitud';
+import {Velocidad} from '../src/ejercicio-2/velocidad';
 
 describe('Ejercicio 2: Conversor de unidades', () => {
     var longitud1 = new Longitud(20)
     var longitud2 = new Longitud(2.5)
+
+    var velocidad1 = new Velocidad(120)
 
     describe('Prueba de la clase longitud', () => {
         it('longitud1.convert("Kilometros a Metros") returns value 20000.0', () => {
@@ -15,8 +18,18 @@ describe('Ejercicio 2: Conversor de unidades', () => {
             expect(longitud1.convert("Centimetros a Metros")).to.be.equal(0.2);
         });
 
-        it('longitud1.convert("Kilometros a Centimetros") returns value 250000', () => {
+        it('longitud2.convert("Kilometros a Centimetros") returns value 250000', () => {
             expect(longitud2.convert("Kilometros a Centimetros")).to.be.equal(250000);
+        });
+    });
+
+    describe('Prueba de la clase velocidad', () => {
+        it('velocidad1.convert("Kilometros/hora a Metros/segundo") returns value 33.333333333333336', () => {
+          expect(velocidad1.convert("Kilometros/hora a Metros/segundo")).to.be.equal(33.333333333333336);
+        });
+
+        it('velocidad1.convert("Millas/hora a Kilometros/hora") returns value 193.07999999999998', () => {
+            expect(velocidad1.convert("Millas/hora a Kilometros/hora")).to.be.equal(193.07999999999998);
         });
     });
 
